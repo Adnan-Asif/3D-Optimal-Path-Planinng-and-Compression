42e73cdc354db53fa588cd2cb044bf7cee280c01
batool-ahmed <60252455+batool-ahmed@users.noreply.github.com>
2021-05-27 20:37:47 +0500

    Update obj.py

--- src/obj.py
+++ src/obj.py
@@ -1,12 +1,22 @@
+#This file contains all the classes and faces used to obtain the object files, store them as octrees and then display their original or compressed versions on blender
 #import bpy
 
 def get_edges(length):
+    '''
+    This function takes in the length and returns the edge list in the ascending order of vertex numbers
+    The edges list is a nested list with tuples containing two connected vertices' index in the vertices list
+    '''
     edges = []
     for i in range(0,length-1):
         edges.append((i,i+1))
     return edges 
 
 def get_Vertices_Faces(regions):
+    '''
+    This function takes in the regions and returns a nested tuple (vertices, faces) containing the vertices and faces list of the regions.
+    Vertices = List of (x,y,z) points
+    Faces = List of faces with nested tuples (p1, p2, p3, p4) containing the vertices making up that face
+    '''
     region_vertices = []
     region_faces = []
     #region_edges = []
@@ -63,6 +73,9 @@ def get_Vertices_Faces(regions):
     return (region_vertices,region_faces)
 
 def display(vertices, edges = None, faces = None) -> None:
+    '''
+    This function takes in the vertices (compulsory), and edges and/or faces (optional) to form a mesh with them and subsequently display that mesh
+    '''
     mymesh = bpy.data.meshes.new("obj")
     myobj = bpy.data.objects.new("obj", mymesh)
     myobj.location = bpy.context.scene.cursor.location
@@ -82,8 +95,18 @@ def display(vertices, edges = None, faces = None) -> None:
     mymesh.polygons.foreach_set("use_smooth", values)
 
 class obj:
-    
+    '''
+    This class is for the objects.
+    This class stores the vertices of the object.
+    Note that we have stored the vertices because we wanted to compare the original and compressed versions.
+    '''
     def __init__(self, path: str) -> None :
+        '''
+        Arguments: 
+        - path: path of the .obj file
+        
+        This function reads the .obj file at the given path and stores its vertices
+        '''
         self.vertices = []
         #self.faces = []
         
@@ -94,6 +117,7 @@ class obj:
                 vertex = line.split()
                 #print(vertex)
                 self.vertices.append((float(vertex[1]), float(vertex[2]), float(vertex[3])))
+      
             '''
             elif line[0:] == "f ":
                 face = line.split()
@@ -107,22 +131,53 @@ class obj:
 
     
     def get_vertices(self):
+        '''
+        Arguments:
+        None
+        
+        Returns: nothing
+        
+        This function returns the object's vertices list
+        '''
         return self.vertices
     
     
     def store(self, tree: Octree) -> None :
+        '''
+        Arguments:
+        tree = the octree to store this object in
+        
+        Returns: nothing
+        
+        This function adds the vertices to the (oct)tree passed to it.
+        '''
         for vertex in self.vertices:
             tree.Add(Point(vertex[0], vertex[1], vertex[2]))
         
    
         
 '''
-oct = Octree(-10, -10, -10, 10, 10, 10)    
-t1 = obj("C:\\Users\\batoo\\Documents\\HU\\DS2\\map_1.obj")
+oct = Octree(-10, -10, -10, 10, 10, 10) 
+#storing an object of obj class with its source .obj file
+t1 = obj(file_address) #put in the complete address of the .obj file 
 t1.store(oct)                        
 
+#This part below is used to display the original object (i.e. with its original coordinates)
+vertices = t1.get_vertices()
+display(vertices)
+
+#The part below is used to display the vertices and regions of the octree child at a certain level
+level = 10
+vertices = oct.level_points(level, [])
+display(vertices)
+regions = oct.getOctreeRegions(level, [])
+region_vertices, region_faces = get_Vertices_Faces(regions)
+display(region_vertices, None, region_faces)
+
+#The part below is used to generate the from one point (starting) to another(goal)
 EmptyRegions =  oct.getEmptyRegions(5,[]) #loading empty regions
 Graph = createGraph(EmptyRegions) #creating graph of empty regions
+
 #now defining starting and goal points in the form (x,y,z)
 
 Starting = (10, -25, -50)
@@ -131,6 +186,7 @@ Goal = (80, 40, 50)
 x = getClosestEmptyRegion(Graph, Starting)
 y = getClosestEmptyRegion (Graph, Goal) 
 
+#path represents the regions traversed to reach the end point while path2 represents the exact vertices in this traversal
 path = list()
 path = FindShortestPath(Graph, x, y)
 
