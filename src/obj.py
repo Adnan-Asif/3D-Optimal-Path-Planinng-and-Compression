6230787ef1d96de2178c140383a1ce0e8a257e3c
batool-ahmed <60252455+batool-ahmed@users.noreply.github.com>
2021-05-27 06:29:09 +0500

    Update obj.py

--- src/obj.py
+++ src/obj.py
@@ -1,5 +1,76 @@
 #import bpy
 
+def get_Vertices_Faces(regions):
+    region_vertices = []
+    region_faces = []
+    #region_edges = []
+    
+    ind = 0
+            
+    for region in regions:
+        p1 = region[0]
+        p8 = region[1]
+        p2 = (p8[0],p1[1],p1[2])
+        p3 = (p1[0],p8[1],p1[2])
+        p4 = (p1[0],p1[1],p8[2])
+        p5 = (p8[0],p8[1],p1[2])
+        p6 = (p8[0],p1[1],p8[2])
+        p7 = (p1[0],p8[1],p8[2])
+                
+
+        region_vertices.append(p1)
+        region_vertices.append(p2)
+        region_vertices.append(p3)
+        region_vertices.append(p4)
+        region_vertices.append(p5)
+        region_vertices.append(p6)
+        region_vertices.append(p7)
+        region_vertices.append(p8)
+                
+                
+        region_faces.append((ind, ind+1, ind+4, ind+2))
+        region_faces.append((ind+1, ind+5, ind+7, ind+4))
+        region_faces.append((ind, ind+3, ind+6, ind+2))
+        region_faces.append((ind+5, ind+3, ind+6, ind+7))
+        region_faces.append((ind+2, ind+4, ind+7, ind+6))
+        region_faces.append((ind, ind+1, ind+5, ind+3))
+                
+        '''
+        region_edges.append((ind, ind+1))
+        region_edges.append((ind, ind+2))
+        region_edges.append((ind, ind+3))
+        region_edges.append((ind+1, ind+4))
+        region_edges.append((ind+1, ind+5))
+        region_edges.append((ind+2, ind+4))
+        region_edges.append((ind+2, ind+7))
+        region_edges.append((ind+3, ind+5))
+        region_edges.append((ind+3, ind+6))
+        region_edges.append((ind+4, ind+7))
+        region_edges.append((ind+5, ind+7))
+        region_edges.append((ind+6, ind+7))
+                
+        print(region_edges)
+        '''
+
+        ind += 8
+    
+    return (region_vertices,region_faces)
+
+def display(vertices, faces = None) -> None:
+    mymesh = bpy.data.meshes.new("obj")
+    myobj = bpy.data.objects.new("obj", mymesh)
+    myobj.location = bpy.context.scene.cursor.location
+    bpy.context.collection.objects.link(myobj)
+    
+    if faces:
+        mymesh.from_pydata(vertices, [], faces)
+    else:
+        mymesh.from_pydata(vertices, [], [])
+
+    mymesh.update(calc_edges=True)
+    values = [True] * len(mymesh.polygons)
+    mymesh.polygons.foreach_set("use_smooth", values)
+
 class obj:
     
     def __init__(self, path: str) -> None :
@@ -33,85 +104,9 @@ class obj:
         for vertex in self.vertices:
             tree.Add(Point(vertex[0], vertex[1], vertex[2]))
         
-    
-    def display_vertices(self, tree_vertices = None) -> None:
-        mymesh = bpy.data.meshes.new("object")
-        myobj = bpy.data.objects.new("object", mymesh)
-        myobj.location = bpy.context.scene.cursor.location
-        bpy.context.collection.objects.link(myobj)
+   
         
-        if tree_vertices != None:
-            mymesh.from_pydata(tree_vertices, [], [])
-        else:
-            mymesh.from_pydata(self.vertices, [], [])
-        
-        mymesh.update(calc_edges=True)
-        values = [True] * len(mymesh.polygons)
-        mymesh.polygons.foreach_set("use_smooth", values)
-        
-    
-    def display_regions(self, tree_regions) -> None:
-        region_vertices = []
-        region_faces = []
-        #region_edges = []
-        ind = 0
-            
-        for region in tree_regions:
-            p1 = region[0]
-            p8 = region[1]
-            p2 = (p8[0],p1[1],p1[2])
-            p3 = (p1[0],p8[1],p1[2])
-            p4 = (p1[0],p1[1],p8[2])
-            p5 = (p8[0],p8[1],p1[2])
-            p6 = (p8[0],p1[1],p8[2])
-            p7 = (p1[0],p8[1],p8[2])
-                
-
-            region_vertices.append(p1)
-            region_vertices.append(p2)
-            region_vertices.append(p3)
-            region_vertices.append(p4)
-            region_vertices.append(p5)
-            region_vertices.append(p6)
-            region_vertices.append(p7)
-            region_vertices.append(p8)
-                
-                
-            region_faces.append((ind, ind+1, ind+4, ind+2))
-            region_faces.append((ind+1, ind+5, ind+7, ind+4))
-            region_faces.append((ind, ind+3, ind+6, ind+2))
-            region_faces.append((ind+5, ind+3, ind+6, ind+7))
-            region_faces.append((ind+2, ind+4, ind+7, ind+6))
-            region_faces.append((ind, ind+1, ind+5, ind+3))
-                
-            '''
-            region_edges.append((ind, ind+1))
-            region_edges.append((ind, ind+2))
-            region_edges.append((ind, ind+3))
-            region_edges.append((ind+1, ind+4))
-            region_edges.append((ind+1, ind+5))
-            region_edges.append((ind+2, ind+4))
-            region_edges.append((ind+2, ind+7))
-            region_edges.append((ind+3, ind+5))
-            region_edges.append((ind+3, ind+6))
-            region_edges.append((ind+4, ind+7))
-            region_edges.append((ind+5, ind+7))
-            region_edges.append((ind+6, ind+7))
-                
-            print(region_edges)
-            '''
-
-            ind += 8
+region_vertices, region_faces = get_Vertices_Faces(tree_regions)
                         
-        mymesh = bpy.data.meshes.new("regions")
-        myobj = bpy.data.objects.new("regions", mymesh)
-        myobj.location = bpy.context.scene.cursor.location
-        bpy.context.collection.objects.link(myobj)
-                 
-        mymesh.from_pydata(region_vertices, [], region_faces)
-        #mymesh_2.from_pydata(region_vertices, region_edges, region_faces)
 
-        mymesh.update(calc_edges=True)
-        values = [True] * len(mymesh.polygons)
-        mymesh.polygons.foreach_set("use_smooth", values)
    
